c92891773baaed1d301706c2ebe515a028f7d280
xryanmason <31865183+xryanmason@users.noreply.github.com>
2022-07-24 15:24:17 -0400

    Add icon button title text (#11)
    This change introduces a required title prop to the icon button component and uses it to add title/hover text to icon buttons.
    
    Testing Instructions
    ---
    1. Open 'marks
    1. Hover the sort, filter, add, favorite, and edit icon buttons
        - [x] Verify that the buttons have relevant title text
    
    Acceptance Criteria
    ---
    When I hover over an icon button:
    - I can see text explaining what the button is for.

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
 <template>
   <button
     class="hover:bg-stone-300 dark:hover:bg-gray-700 rounded-lg py-2 px-3"
     type="button"
+    :title="title"
   >
     <slot />
   </button>
